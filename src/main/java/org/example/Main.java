a528785e8d5011a9f0599a0ff01ad585a312c406
Nicolas Angel Risso Fontan <nicolasangelrissofontan@MBP-de-Nicolas.fibertel.com.ar>
2024-04-02 16:57:36 -0300

    add comentario y variable constante

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,5 @@
 /*
- * SonarQube, open source software quality management tool.
- * Copyright (C) 2008-2013 SonarSource
- * mailto:contact AT sonarsource DOT com
- *
- * SonarQube is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * SonarQube is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * i obtendra los valores numericos del 1 hasta val y los imprimira con el logger info.
  */
 
 package org.example;
@@ -23,10 +7,10 @@ import java.util.logging.Logger;
 
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
+    private static final int val = 5;
     public static void main(String[] args) {
         logger.info("Hello and welcome!");
 
-        int val = 5;
 
         for (int i = 1; i <= val; i++) {
             logger.info("i = " + i);
